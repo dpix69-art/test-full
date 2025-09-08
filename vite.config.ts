8ad0cfc18fb60ddbeab9710d66ec8ae62fa0a28a
dpix69-art <dpix69@gmail.com>
2025-09-08 12:57:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   base: '/test-full/',   // <— ВАЖНО: со слэшами с двух сторон
+//   plugins: [react()],
+// });
 export default defineConfig({
-  base: '/test-full/',   // <— ВАЖНО: со слэшами с двух сторон
-  plugins: [react()],
+  base: '/test-full/',
+  publicDir: 'public', // ← жёстко укажи
 });
+
