b2546d60b17851e979488cb490c8b2d5f571b285
dpix69-art <dpix69@gmail.com>
2025-09-08 11:26:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,19 @@
 //   },
 // });
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/https://github.com/dpix69-art/test-full/',          // ← замени на имя репозитория
+//   plugins: [react()],
+//   optimizeDeps: { exclude: ['lucide-react'] },
+// });
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/https://github.com/dpix69-art/test-full/',          // ← замени на имя репозитория
+  base: '/test-full/',   // <— ВАЖНО: со слэшами с двух сторон
   plugins: [react()],
-  optimizeDeps: { exclude: ['lucide-react'] },
 });
