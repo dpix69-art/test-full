320e4f8d0332f5ddcb30bca833133371189e8d00
dpix69-art <dpix69@gmail.com>
2025-09-08 13:31:18 +0200

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -17,7 +17,7 @@ const Contact: React.FC = () => {
   };
 
   return (
-    <main className="max-w-4xl mx-auto px-8 py-16">
+    <main className="max-w-4xl mx-auto px-8 py-16 pt-32">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
